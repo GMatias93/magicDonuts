48720c7b364daa4f1efa51bc2ec1e3563a84b4ed
Gabriel Matias <gabriel.2m.deleon@gmail.com>
2015-11-16 12:32:03 -0800

    still working to integrate nested view with homepage multi-view

--- client/app/app.js
+++ client/app/app.js
@@ -17,11 +17,6 @@ angular.module("myApp", [
 			templateUrl: "app/auth/loginAuth.html"
 		})
 		//messages
-		// .state("messages", {
-		// 	controller: "MessagesController",
-		// 	url: "/messages",
-		// 	templateUrl: "app/messages/messages.html"
-		// })
 		//need this for later, in order to have multiple views in one window
 		// https://scotch.io/tutorials/angular-routing-using-ui-router
 		.state("home", {
@@ -29,15 +24,27 @@ angular.module("myApp", [
 			views: {
 				"": { templateUrl: "app/home/home.html" },
 				//html and controller required for onlineUsers view
-				"onlineUsers@home": { template: "online users column!!"},
+				"onlineUsers@home": { template: "online users column!!" },
 				//html and controller required for chatrooms view
-				"chatrooms@home": {
+				"chatrooms@home": { templateUrl: "app/chatrooms/chatrooms.html" },
+				//html and controller required for userSearch view
+				"userSearch@home": { template: "user search section lives here" }
+			}
+		})
+		.state("home.chatroom", {
+			url:"/chatroom",
+			views: {
+				"": { templateUrl: "app/home/home-chatroom.html" },
+				//html and controller required for onlineUsers view
+				"chatroomUsers@home.chatroom": { template: "online users column!!"},
+				//html and controller required for userSearch view
+				"codeShare@home.chatroom": { template: "Code share goes here!!" },
+				//html and controller required for chatrooms view
+				"chat@home.chatroom": {
 					controller: "MessagesController",
 					url: "/messages",
 					templateUrl: "app/messages/messages.html"
-				},
-				//html and controller required for userSearch view
-				"userSearch@home": { template: "user search section lives here" }
+				}
 			}
 		});
 
